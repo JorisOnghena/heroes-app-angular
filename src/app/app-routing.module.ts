fc9ed11ab5b70c5e0b9ad92f89478a37cf65d5e0
Joris <joris.onghena@uantwerpen.be>
2019-06-24 16:11:51 +0200

    Added some example routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'crisis-center', component: CrisisListComponent },
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  /*{
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  },*/
+  { path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  }// ,
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
